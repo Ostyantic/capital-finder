9406020bf38ab8fbb28f40887dc48aaf7a89e780
Anthony Lopez <Antl21292@gmail.com>
2023-05-10 22:22:16 -0400

    slight change

--- api/primary.py
+++ api/primary.py
@@ -5,7 +5,7 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         confirm = "If you can see me, it's working!"
         self.send_response(200)
-        self.send_header('Content-type', 'test/plain')
+        self.send_header('Content-type', 'text/plain')
         self.end_headers()
         self.wfile.write(confirm.encode())
 
